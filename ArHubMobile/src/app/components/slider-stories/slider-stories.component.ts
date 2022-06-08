c0b73b3d88c102edf518f01159967d4bec50aa5b
vladislav <vlee@sbdagroup.com>
2022-06-08 04:27:44 +0400

    fix popular

--- ArHubMobile/src/app/components/slider-stories/slider-stories.component.ts
+++ ArHubMobile/src/app/components/slider-stories/slider-stories.component.ts
@@ -8,17 +8,17 @@ import {PostService} from "../../services/post.service";
   encapsulation: ViewEncapsulation.None,
 })
 export class SliderStoriesComponent implements OnInit {
-  popularPostList1: any;
-  popularPostList2: any;
-  popularPostList3: any;
+  popularPostList1: any = [];
+  popularPostList2: any = [];
 
   constructor(private postService: PostService) { }
 
   ngOnInit(): void {
-    this.postService.getPopularPost().subscribe(value => {
-      this.popularPostList1.push(value[0],value[1]);
-      this.popularPostList2.push(value[2],value[3]);
-      this.popularPostList3.push(value[4],value[5]);
+    this.postService.getPopularPost().subscribe((value: any[]) => {
+      this.popularPostList1.push(value[0]);
+      this.popularPostList1.push(value[1]);
+      this.popularPostList2.push(value[2]);
+      this.popularPostList2.push(value[3]);
     })
   }
 }
